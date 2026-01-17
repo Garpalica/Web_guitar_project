a3b3d3a351f1d09a8dc7e403bbb3a5ebe66ef3b7
Андрей <andrejkurbatov074@gmail.com>
2026-01-17 11:53:43 +1000

    База данных пользователей и Блэклист

--- Backend/app.py
+++ Backend/app.py
@@ -3,20 +3,34 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_cors import CORS
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guitar_base.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '9238hff29ufh92f209j23-9'
+app.config['JWT_SECRET_KEY'] = '834g93gb9ug34u9njscd234kmpiq3jipwuo3v55vu94fpi53foqfm3ipw7vu959uw'
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token_cookie'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 
 api = Api(app)
 db = SQLAlchemy(app)
 
-@app.route('/')
-def hello():
-    return jsonify({"message": "Работает аааа?"})
+class Token_Black_list(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(36), nullable=False, unique=True)
+    revoked_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(255))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
